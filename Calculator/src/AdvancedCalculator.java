2cc5632d74952732a00b761033261e08fafc1134
woohyun-park <qkrdngus1227@gmail.com>
2025-10-16 22:35:02 +0900

    AdvancedCalculator 주석 추가 완료

--- Calculator/src/AdvancedCalculator.java
+++ Calculator/src/AdvancedCalculator.java
@@ -1,11 +1,17 @@
+// 계산기 v2.0 관련 클래스
+// https://github.com/park-woohyun/Java-Calculator-rt/issues/1
 public class AdvancedCalculator extends Calculator {
 
+    // 계산기 v2.0 곱하기 개발 완료
+    // https://github.com/park-woohyun/Java-Calculator-rt/issues/2
     @Override
     public int multiply() {
         result = num1 * num2;
         return result;
     }
 
+    // 계산기 v2.0 나눗셈 개발 완료
+    // https://github.com/park-woohyun/Java-Calculator-rt/issues/3
     @Override
     public int divide() {
         result = num1 / num2;
